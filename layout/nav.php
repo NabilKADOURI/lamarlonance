281bfba894273d4add573ebdf3ffe62a74fb99f9
Nabil KADOURI <kadourinabil7@gmail.com>
2024-08-05 11:09:19 +0200

    edit navbar

--- layout/nav.php
+++ layout/nav.php
@@ -3,29 +3,17 @@
 require_once "functions/getMessage.php";
 ?>
 
-
-
-<nav class="navbar navbar-expand-lg navbar-light ">
-  <div class="container">
-    <button
-      data-mdb-collapse-init
-      class="navbar-toggler"
-      type="button"
-      data-mdb-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <i class="fas fa-bars"></i>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      
-      <a class="navbar-brand mt-2 mt-lg-0" href="#">
+<section id="navbar">
+    <nav class="navbar navbar-expand-lg shadow  fixed-top bg bg-white">
+        <div class="container">
+        <a class="navbar-brand mt-2 mt-lg-0" href="#">
         <img src="/lamarlonance/Image/logo-lamarlonance.png" height="60" alt="Logo lamarlonance" loading="lazy"/>
       </a>
-      
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 d-flex gap-5">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 d-flex gap-5">
         <li class="nav-item">
           <a class="nav-link lobster" href="index.php">Accueil</a>
         </li>
@@ -42,9 +30,10 @@ require_once "functions/getMessage.php";
           <a class="nav-link lobster" href="contact.php">Contact</a>
         </li>
       </ul>
-    </div>
-</nav>
 
-
+            </div>
+        </div>
+    </nav>
+</section>
 
 
